9ecb39f0f70cca298636170b4e3b281bf71f51b4
Aleksander Brol <brol.aleksander@gmail.com>
2016-01-28 03:20:45 +0100

    [RentBundle:Client]Wygenerowano z crud: kontroler, widoki i formularz dla modelu RentBundle:Client

---
+++ src/RentBundle/Form/ClientType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('imie')
+            ->add('nazwisko')
+            ->add('rents')
+            ->add('comments')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RentBundle\Entity\Client'
+        ));
+    }
+}
