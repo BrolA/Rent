2ec399642f4b962af1ec5df91304435ccbaeff48
Aleksander Brol <brol.aleksander@gmail.com>
2016-01-28 21:08:17 +0100

    [poprawiona]LoadClientData poprawna

--- src/RentBundle/DataFixtures/ORM/LoadClientData.php
+++ src/RentBundle/DataFixtures/ORM/LoadClientData.php
@@ -6,24 +6,48 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use RentBundle\Entity\Client;
 
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+class LoadClientData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new Client();
-        $userAdmin->setUsername('client');
-        $userAdmin->setPassword('test');
+        $client1 = new Client();
+        $client1->setImie('Jan');
+        $client1->setNazwisko('Kowalski');
 
-        $manager->persist($userAdmin);
+        $client2 = new Client();
+        $client2->setImie('Jan');
+        $client2->setNazwisko('Nowak');
+        
+        $client3 = new Client();
+        $client3->setImie('John');
+        $client3->setNazwisko('Smith');
+
+        $client4 = new Client();
+        $client4->setImie('Janusz');
+        $client4->setNazwisko('Kowal');
+
+        $client5 = new Client();
+        $client5->setImie('Grażyna');
+        $client5->setNazwisko('Kowal');
+        
+        $manager->persist($client1);
+        $manager->persist($client2);
+        $manager->persist($client3);
+        $manager->persist($client4);
+        $manager->persist($client5);
         $manager->flush();
 
-        $this->addReference('client-user', $userAdmin);
+        $this->addReference('client1-user', $client1);
+        $this->addReference('client2-user', $client2);
+        $this->addReference('client3-user', $client3);
+        $this->addReference('client4-user', $client4);
+        $this->addReference('client5-user', $client5);
     }
 
     public function getOrder()
     {
         // the order in which fixtures will be loaded
         // the lower the number, the sooner that this fixture is loaded
-        return 1;
+        return 2;
     }
 }
