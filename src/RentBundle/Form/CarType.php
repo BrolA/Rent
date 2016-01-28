4965a07af41a09f03681a604855e4e42edbc383e
Aleksander Brol <brol.aleksander@gmail.com>
2016-01-28 03:15:44 +0100

    [RentBundle:Car]Wygenerowano z crud: kontroler, widoki i formularz dla modelu RentBundle:Car

---
+++ src/RentBundle/Form/CarType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('zdjecie')
+            ->add('model')
+            ->add('kategoria')
+            ->add('kolor')
+            ->add('cenaDoba')
+            ->add('wypozyczony')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RentBundle\Entity\Car'
+        ));
+    }
+}
