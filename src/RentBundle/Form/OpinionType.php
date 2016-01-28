13c306408b52c2e631a45e22f039a4ac1fa0100e
Aleksander Brol <brol.aleksander@gmail.com>
2016-01-28 03:24:41 +0100

    [RentBundle:Opinion]Wygenerowano z crud: kontroler, widoki i formularz dla modelu RentBundle:Opinion

---
+++ src/RentBundle/Form/OpinionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OpinionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('tytul')
+            ->add('dataDodania', 'date')
+            ->add('opinia')
+            ->add('car')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RentBundle\Entity\Opinion'
+        ));
+    }
+}
